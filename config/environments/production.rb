21c0b083fe75c573bbe237454f63f513ff7e36e9
nikita <MULLOVSKII@MAIL.ru>
2016-07-17 15:16:44 +0300

    config done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.web_socket_server_url = "wss://whoyase.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://whoyase.herokuapp.com', 'https://whoyase.herokuapp.com']
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
